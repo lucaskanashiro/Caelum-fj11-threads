5dc294bd5c9a8e7d3028a30bd6cc29d67751810f
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-07-03 19:14:45 -0300

    Seção 17.8 - Exercício 4

---
+++ src/exercicio4/BancoDeDadosUsandoHashSet.java
@@ -0,0 +1,43 @@
+package exercicio4;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class BancoDeDadosUsandoHashSet {
+
+	public static void main(String[] args) throws InterruptedException{
+		Collection<String> sqls = new HashSet<String>();
+		
+		ProduzSQL p1 = new ProduzSQL(0, 10000, sqls);
+		Thread t1 = new Thread(p1);
+		
+		ProduzSQL p2 = new ProduzSQL(10000, 20000, sqls);
+		Thread t2 = new Thread(p2);
+		
+		ProduzSQL p3 = new ProduzSQL(20000, 30000, sqls);
+		Thread t3 = new Thread(p3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		t1.join();
+		t2.join();
+		t3.join();
+
+		System.out.println("Threads produtoras de SQLs finalizadas!");
+		
+		for (int i = 0; i < 15000; i++) {
+			if (!sqls.contains("SQL" + i)) {
+				throw new IllegalStateException("não encontrei a SQL" + i);
+			}
+		}
+		
+		if (sqls.contains(null)) {
+			throw new IllegalStateException("não devia ter null aqui dentro!");
+		}
+		
+		System.out.println("Fim da execucao com sucesso");
+
+	}
+}
